52d6edf31e8c6f3913fe5099e5139dc0569ae704
cwher <cwher@outlook.com>
2021-12-22 22:09:43 +0800

    fix: fix bug in linear regression

--- lab5/src/P1.py
+++ lab5/src/P1.py
@@ -5,10 +5,11 @@ if __name__ == "__main__":
     X = np.load('Data1_X.npy')
     Y = np.load('Data1_Y.npy')
     """Your code here"""
+    X = np.hstack([X, np.ones([X.shape[0], 1])])
     Y_hat = X @ np.linalg.inv((X.T @ X)) @ X.T @ Y
     ic(Y_hat)
 
     Y_perp = Y - Y_hat
-    for i in range(5):
+    for i in range(6):
         x = X[:, i].reshape(-1, 1)
         ic(np.vdot(x, Y_perp))
